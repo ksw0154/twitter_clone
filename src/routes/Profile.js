9e5d1705d934794b6bc5e2b1e5de573b2dd42cd3
ksw0154 <rlatkddnjs44@naver.com>
2022-02-06 01:00:09 +0900

    Log Out

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+import { authService } from "../fbase";
 
 const Profile = () => {
-  return <span>Profile</span>;
+  let history = useHistory();
+
+  const onLogOutClick = () => {
+    authService.signOut();
+    history.push("/");
+  };
+
+  return (
+    <>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  );
 };
 
 export default Profile;
